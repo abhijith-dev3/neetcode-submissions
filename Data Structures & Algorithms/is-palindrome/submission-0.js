977b22063d389541b610475644a681df35788bbb
Kairo🐼 <abhijith4389@gmail.com>
2026-05-05 12:30:21 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.js
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+        let left = 0; 
+        let right = s.length - 1;
+
+        while (left < right){
+            while(left < right && !isAlphaNum(s[left])){ // if it not a letter/number left++ next chara or number
+                left++
+            }
+
+            while(left < right && !isAlphaNum(s[right])){
+                right--
+            }
+
+            if(s[left].toLowerCase() !== s[right].toLowerCase()){
+                return false;
+            }
+            left++
+            right--
+        }
+        return true;
+    }   
+}
+function isAlphaNum(c){
+        return /[a-z0-9]/i.test(c)}
