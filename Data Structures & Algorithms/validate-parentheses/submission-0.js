398b51e8e07e0a63df62288edfe282805f04e5a4
Abhi <abhijith4389@gmail.com>
2026-05-31 22:44:39 -0900

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.js
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s) {
+       const stack = [];
+       const map = {
+        ")" : "(",
+        "]" : "[",
+        "}" : "{"
+       }
+
+       for(let char of s){
+        if(char === "(" || char === "[" || char==="{"){
+            stack.push(char)
+        }else{
+            if(stack.length === 0){
+                return false;
+            }
+
+            if(stack.pop() !== map[char]){
+                return false
+            }
+        }
+       }
+       return stack.length === 0;
+    }
+}
